b9eabc95b1d79f184d347d7bbca56cae129aefb5
jonathanvra <jonathan.vra@gmail.com>
2025-09-15 15:56:09 -0500

    doc(inserts_directos): Agregar comentarios para mayor claridad en las inserciones directas

--- sql/extras/inserts_directos.sql
+++ sql/extras/inserts_directos.sql
@@ -1,8 +1,10 @@
 -- Inserciones directas opcionales (sin SP)
 
+-- Inserta un docente directamente sin usar procedimiento
 INSERT INTO docente (numero_documento, nombres, titulo, anios_experiencia, direccion, tipo_docente)
 VALUES ('CC3001','Jorge Torres','MSc. Ing. Software',8,'Cra 45 # 12-34','Tiempo completo');
 
+-- Inserta un proyecto asignado al docente recién creado
 INSERT INTO proyecto (nombre, descripcion, fecha_inicial, fecha_final, presupuesto, horas, docente_id_jefe)
 VALUES ('Sistema Nómina','Módulo RRHH','2025-04-01',NULL, 15000000, 500,
         (SELECT docente_id FROM docente WHERE numero_documento='CC3001'));
